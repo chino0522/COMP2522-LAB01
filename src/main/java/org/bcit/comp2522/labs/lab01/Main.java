f712a20f79af5bdecc6b6278a6a753485c929b91
chino0522 <baekjinho0522@gmail.com>
2023-01-03 23:28:57 -0800

    fixed line

--- src/main/java/org/bcit/comp2522/labs/lab01/Main.java
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -3,5 +3,6 @@ package org.bcit.comp2522.labs.lab01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello 2522");
     }
 }
