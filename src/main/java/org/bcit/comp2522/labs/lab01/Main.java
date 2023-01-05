b5eccff04d80fd307365530e10b208024677bd1e
chino0522 <baekjinho0522@gmail.com>
2023-01-05 02:05:18 -0800

    finished upto TODO10, drawing is the next step

--- src/main/java/org/bcit/comp2522/labs/lab01/Main.java
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -1,7 +1,17 @@
 package org.bcit.comp2522.labs.lab01;
 
 public class Main {
+
+    public int factorial(int num) {
+        if (num <= 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
     public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.factorial(5));
         System.out.println("Hello World");
         System.out.println("Hello 2522");
     }
---
+++ src/main/java/org/bcit/comp2522/labs/lab01/Window.java
@@ -0,0 +1,25 @@
+package org.bcit.comp2522.labs.lab01;
+
+import processing.core.PApplet;
+
+public class Window extends PApplet {
+
+    public void settings() {
+        size(500, 500);
+    }
+
+    public void draw() {
+//        ellipse(mouseX, mouseY, 10, 10);
+        circle(250, 250, 100);
+    }
+
+    public void mousePressed() {
+        background(64);
+    }
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"window"};
+        Window window = new Window();
+        PApplet.runSketch(processingArgs, window);
+    }
+}
