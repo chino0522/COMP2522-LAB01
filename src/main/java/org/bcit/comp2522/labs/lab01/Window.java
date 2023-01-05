b8663bfa3a08ba576340ac21d9f0926e88150d3f
chino0522 <baekjinho0522@gmail.com>
2023-01-05 03:48:15 -0800

    fished drawing a snowman

--- src/main/java/org/bcit/comp2522/labs/lab01/Window.java
+++ src/main/java/org/bcit/comp2522/labs/lab01/Window.java
@@ -10,7 +10,22 @@ public class Window extends PApplet {
 
     public void draw() {
 //        ellipse(mouseX, mouseY, 10, 10);
-        circle(250, 250, 100);
+        rect(100, 250, 120, 10);
+        rect(275, 250, 120, 10);
+        rect(300, 350, 10, 120);
+        rect(200, 350, 10, 120);
+        circle(250, 300, 180);
+        circle(250, 160, 100);
+        circle(250, 250, 10);
+        circle(250, 300, 10);
+        circle(250, 350, 10);
+        circle(230, 150, 15);
+        circle(270, 150, 15);
+        triangle(200, 170, 250, 165, 250, 175);
+        triangle(220, 120, 250, 80, 280, 120);
+        line(220, 190, 280, 190);
+//        square(200, 200, 200);
+//        triangle(150, 200, 350, 200, 350, 150);
     }
 
     public void mousePressed() {
